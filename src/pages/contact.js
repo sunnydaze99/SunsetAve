d47ca0658d4b73763dcf29db2e945d239c7dbede
sunnydaze99 <tosin.bangudu73@gmail.com>
2025-01-10 16:03:24 -0800

    Initial commit 1/10/25

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/main.scss';
+
+
+function Contact() {
+    return (
+        <div className="hero">
+            <h1>Contact Page</h1>
+            <p>Stay tuned for amazing discounts!</p>
+        </div>
+    );
+}
+
+export default Contact;
